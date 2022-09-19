7ae6e68bfa3403c261ba59b8b5882c8b5eeb96f5
chandan mohapatra <get2chandanmohapatra@gmail.com>
2022-09-19 16:59:24 +0530

    Second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              Crypto Hunter
+             Angels coin
             </Typography>
             <Select
               variant="outlined"
